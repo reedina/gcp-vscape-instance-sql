def017bbcafe373db1c0e30b9d0284b18999e58b
Reedina <reedinafarms@gmail.com>
2017-11-27 14:17:37 -0800

    updated auth_accounts to include role;

--- auth_accounts.sql
+++ auth_accounts.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS auth_accounts (
   id int not null auto_increment primary key,
   email varchar(200) unique,
-  password varchar(200)
+  password varchar(200),
+  role_id int
 );
